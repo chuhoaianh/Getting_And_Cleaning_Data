5c0e911f1fe4adc935dd2b92a223014f5d2aa794
chuhoaianh <chuhoaianh@gmail.com>
2015-07-18 11:45:02 -0600

    second version - better output data set format

--- run_analysis.R
+++ run_analysis.R
@@ -29,18 +29,27 @@ for (i in 1:nrow(Y_data)){
       Y_data[i,1] = as.character(activity_labels[j,2])
   }
 }
-  
-names(Y_data) <- "Activities"
+names(Y_data) <- "activities"
+
 
 #4. Appropriately labels the data set with descriptive variable names. 
-names(Subject_data) <- "Subject"
+names(Subject_data) <- "subject"
+
+#Cleanup Y_data values
+Y_data <- as.data.frame(sapply(Y_data, tolower))  #set lower case all values
+Y_data <- as.data.frame(sapply(Y_data, gsub, pattern="_", replacement=" ")) #remove "_"
+
+#Cleanup X_data column names
+names(X_data) <- tolower(names(X_data)) #set lowever case all values
+
+#create tidy data set
 tidy_data <- cbind(Subject_data, Y_data, X_data)
-write.table(tidy_data, "MergedClean_dataset.txt", row.names = FALSE)
+
 
 #5. From the data set in step 4, creates a second, independent tidy data set with the average 
 #of each variable for each activity and each subject.
-subjects <- unique(tidy_data$Subject)
-activities <- unique(tidy_data$Activities)
+subjects <- unique(tidy_data$subject)
+activities <- unique(tidy_data$activities)
 tidy_data2 <- tidy_data[1,] #initialize new data set
 
 k <- 1
@@ -50,7 +59,7 @@ for (i in 1:length(subjects)){  #loop thru the subjects list
     tidy_data2[k, 2] <- activities[j] #assign the activities lable to the current row of the new data set
     
     #create a temp data frame with all the match current subject and activities label
-    temp <- tidy_data[tidy_data$Subject==i & tidy_data$Activities==activities[j], ]
+    temp <- tidy_data[tidy_data$subject==i & tidy_data$activities==activities[j], ]
     
     #calculate means of every columns from the temp data frame and put them to the new data set
     tidy_data2[k, 3:length(tidy_data)] <- colMeans(temp[, 3:length(tidy_data)])
